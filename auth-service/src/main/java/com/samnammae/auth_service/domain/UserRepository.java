cc7532bc714d3a85cd900ed7b5898a0b94b8a3ac
jho7535 <jho7535@naver.com>
2025-04-25 23:00:10 +0900

    feat : user 리포지토리 생성

---
+++ auth-service/src/main/java/com/samnammae/auth_service/domain/UserRepository.java
@@ -0,0 +1,14 @@
+package com.samnammae.auth_service.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 사용자 조회
+    Optional<User> findByEmail(String email);
+
+    // 이메일 중복 체크
+    boolean existsByEmail(String email);
+}
