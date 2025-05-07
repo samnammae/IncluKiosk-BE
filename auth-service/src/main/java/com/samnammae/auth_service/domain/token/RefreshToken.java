60f317c87e7b36e9ff80d2e8d2048d439893c140
jho7535 <jho7535@naver.com>
2025-05-07 15:12:18 +0900

    feat : 리프레쉬 토큰 엔터티, 리포지토리 생성

---
+++ auth-service/src/main/java/com/samnammae/auth_service/domain/token/RefreshToken.java
@@ -0,0 +1,34 @@
+package com.samnammae.auth_service.domain.token;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저의 PK (User 엔터티의 id)
+    @Column(nullable = false, unique = true)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime createdAt;
+
+    public void updateToken(String newToken, LocalDateTime newExpiresAt) {
+        this.token = newToken;
+        this.expiresAt = newExpiresAt;
+    }
+}
