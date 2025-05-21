226ec182e9e7fc8a299428ac88e7591c02745684
jho7535 <jho7535@naver.com>
2025-05-21 11:23:08 +0900

    chore : 공통 모듈 수정

--- common-module/src/main/java/com/samnammae/common/exception/ErrorCode.java
+++ common-module/src/main/java/com/samnammae/common/exception/ErrorCode.java
@@ -7,9 +7,9 @@ public enum ErrorCode {
     // Auth Service
     DUPLICATE_EMAIL(400, "이미 등록된 이메일입니다."),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
-    INVALID_PASSWORD(401, "비밀번호가 일치하지 않습니다.")
-    // 필요한 에러 코드 계속 추가
-    ;
+    INVALID_PASSWORD(401, "비밀번호가 일치하지 않습니다."),
+    INVALID_TOKEN(401, "토큰이 유효하지 않습니다."),
+    TOKEN_MISSING(401, "토큰이 존재하지 않습니다.");
 
     private final int status;
     private final String message;
