4fc3d04a11294f5628d654f44329f1b4b730a64a
jho7535 <jho7535@naver.com>
2025-05-21 10:18:04 +0900

    feat : 스웨거 설정 파일 생성 (#8)

---
+++ auth-service/src/main/java/com/samnammae/auth_service/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.samnammae.auth_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components()
+                        .addSecuritySchemes("Authorization", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)));
+    }
+}
