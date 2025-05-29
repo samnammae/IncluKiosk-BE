ae61b80bd5d7791bc9aee26765ca11aa8a11cdd0
jho7535 <jho7535@naver.com>
2025-05-29 22:55:49 +0900

    feat : Store 리포지토리 생성 (#12)

---
+++ admin-service/src/main/java/com/samnammae/admin_service/domain/store/StoreRepository.java
@@ -0,0 +1,6 @@
+package com.samnammae.admin_service.domain.store;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
