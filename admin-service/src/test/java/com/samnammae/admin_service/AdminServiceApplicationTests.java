d6a605c48bb242f286adef3c5489d630362d839c
jho7535 <jho7535@naver.com>
2025-08-10 22:16:25 +0900

    test : ActiveProfiles 어노테이션 적용

--- admin-service/src/test/java/com/samnammae/admin_service/AdminServiceApplicationTests.java
+++ admin-service/src/test/java/com/samnammae/admin_service/AdminServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.samnammae.admin_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AdminServiceApplicationTests {
 
 	@Test
