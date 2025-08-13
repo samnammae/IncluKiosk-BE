23649f7f038309c972f5fcd9925c0c66edf7d1a3
jho7535 <jho7535@naver.com>
2025-08-13 21:17:45 +0900

    chore : 각 서비스 CORS URL 추가

--- auth-service/src/main/java/com/samnammae/auth_service/config/WebConfig.java
+++ auth-service/src/main/java/com/samnammae/auth_service/config/WebConfig.java
@@ -10,11 +10,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해
-                .allowedOrigins("http://desktop-9aub5vr:8081",
-                        "http://localhost:8000",
+                .allowedOrigins("http://localhost:8000",
                         "http://inclukiosk-fe.s3-website.ap-northeast-2.amazonaws.com",
                         "http://localhost:5173",
-                        "http://localhost:3000") // 허용할 프론트 주소
+                        "http://localhost:3000",
+                        "http://localhost:4200",
+                        "http://127.0.0.1:3000",
+                        "http://dap3hz5p3144z.cloudfront.net",
+                        "https://dap3hz5p3144z.cloudfront.net",
+                        "http://ec2-3-36-56-69.ap-northeast-2.compute.amazonaws.com:3000",
+                        "http://inclukiosk.com",
+                        "https://inclukiosk.com",
+                        "http://www.inclukiosk.com",
+                        "https://www.inclukiosk.com"
+                ) // 허용할 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
