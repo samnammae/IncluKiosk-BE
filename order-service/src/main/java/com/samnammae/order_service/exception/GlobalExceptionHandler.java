80cf6375ea4fbe56d02d7c001005f5638c411e40
jho7535 <jho7535@naver.com>
2025-08-18 19:59:48 +0900

    feat : 글로벌 익셉션 핸들러 생성 (#39)

---
+++ order-service/src/main/java/com/samnammae/order_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.samnammae.order_service.exception;
+
+import com.samnammae.common.exception.CustomException;
+import com.samnammae.common.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ApiResponse.error(
+                        e.getErrorCode().getStatus(),
+                        e.getErrorCode().getMessage()
+                ));
+    }
+}
