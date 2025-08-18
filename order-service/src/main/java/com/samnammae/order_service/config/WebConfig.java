b82be35a9ac4f81761acee5571359b2f90f1a41f
jho7535 <jho7535@naver.com>
2025-08-18 17:29:57 +0900

    feat : 웹 설정 클래스 파일 생성 (#39)

---
+++ order-service/src/main/java/com/samnammae/order_service/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.samnammae.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로에 대해
+                .allowedOrigins("http://localhost:8000",
+                        "http://inclukiosk-fe.s3-website.ap-northeast-2.amazonaws.com",
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "http://localhost:4200",
+                        "http://127.0.0.1:3000",
+                        "http://dap3hz5p3144z.cloudfront.net",
+                        "https://dap3hz5p3144z.cloudfront.net",
+                        "http://ec2-3-36-56-69.ap-northeast-2.compute.amazonaws.com:3000",
+                        "http://inclukiosk.com",
+                        "https://inclukiosk.com",
+                        "http://www.inclukiosk.com",
+                        "https://www.inclukiosk.com"
+                ) // 허용할 프론트 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
