91984aba2f43bf8f19f282069aa084582198a97d
jho7535 <jho7535@naver.com>
2025-09-14 22:42:31 +0900

    feat : 매장별 주문 생성일 기준 내림차순 조회 쿼리 메서드 추가

--- order-service/src/main/java/com/samnammae/order_service/domain/order/OrderRepository.java
+++ order-service/src/main/java/com/samnammae/order_service/domain/order/OrderRepository.java
@@ -2,5 +2,10 @@ package com.samnammae.order_service.domain.order;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends MongoRepository<Order, String> {
+
+    // 특정 매장의 주문을 생성일 기준 내림차순으로 조회
+    List<Order> findByStoreIdOrderByCreatedAtDesc(Long storeId);
 }
