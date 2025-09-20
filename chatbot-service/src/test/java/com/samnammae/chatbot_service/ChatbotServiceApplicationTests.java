e055462a82c19417c0b00038c1c1550ccff332a3
jho7535 <jho7535@naver.com>
2025-09-20 20:28:19 +0900

    faet : Chatbot Service 프로젝트 생성 (#46)

---
+++ chatbot-service/src/test/java/com/samnammae/chatbot_service/ChatbotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samnammae.chatbot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
