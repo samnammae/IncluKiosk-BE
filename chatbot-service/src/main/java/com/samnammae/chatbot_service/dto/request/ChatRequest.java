e5a14e541149c5fe732cd1a917f07028ddbcfedc
jho7535 <jho7535@naver.com>
2025-09-20 22:46:14 +0900

    feat : 채팅 요청, 응답 DTO 생성 (#46)

---
+++ chatbot-service/src/main/java/com/samnammae/chatbot_service/dto/request/ChatRequest.java
@@ -0,0 +1,11 @@
+package com.samnammae.chatbot_service.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatRequest {
+    private String sessionId;
+    private String message;
+}
